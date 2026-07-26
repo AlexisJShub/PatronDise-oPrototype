9193a1e46addc3dc87ab3566e87c9e03acd6704f
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-25 18:15:45 -0600

    004 feat Se termino el modelo de empleado implementando dentro de el, el metodo clonar

--- src/org/joaquinsanchez/model/Empleado.java
+++ src/org/joaquinsanchez/model/Empleado.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.joaquinsanchez.model;
 
-/**
- *
- * @author joaqu
- */
-public class Empleado {
-    
+public class Empleado implements Prototype, Cloneable {
+
+    private int id;
+    private String nombre;
+    private String puesto;
+    private double salario;
+
+    public Empleado(int id, String nombre, String puesto, double salario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    // Aca implementamos el metodo clonar con object.clone()
+    @Override
+    public Prototype clonar() {
+        try {
+            return (Empleado) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Error al clonar el empleado", e);
+        }
+    }
+
+    // Nuestros getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{id=" + id + ", nombre='" + nombre + "', puesto='"
+                + puesto + "', salario=" + salario + "}";
+    }
 }
