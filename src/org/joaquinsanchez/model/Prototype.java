2863afb36eeaa77e69c84f2f4909eb0e7fdcbc87
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-25 18:12:20 -0600

    003 feat Agregamos nuestra orden de clonado para que prototype funcione

--- src/org/joaquinsanchez/model/Prototype.java
+++ src/org/joaquinsanchez/model/Prototype.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.joaquinsanchez.model;
 
-/**
- *
- * @author joaqu
- */
-public class Prototype {
-    
+//Aca hacemos la interfaz que define nuestro contrato con prototype
+
+public interface Prototype {
+    Prototype clonar();
 }
