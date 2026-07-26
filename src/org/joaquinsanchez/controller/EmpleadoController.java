514e9babc0c927ed6559a04c4e49c0f45494fc43
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-25 18:22:15 -0600

    005 feat Se concreto el controller de empleado implementando ya el metodo clonar para usarlo con empleado y hacer una copía

--- src/org/joaquinsanchez/controller/EmpleadoController.java
+++ src/org/joaquinsanchez/controller/EmpleadoController.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.joaquinsanchez.controller;
 
-/**
- *
- * @author joaqu
- */
+import org.joaquinsanchez.model.Empleado;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmpleadoController {
-    
+
+    private List<Empleado> empleados;
+
+    public EmpleadoController() {
+        this.empleados = new ArrayList<>();
+    }
+
+    // Creamos un empleado original y lo agregamos a la lista
+    public Empleado crearEmpleado(int id, String nombre, String puesto, double salario) {
+        Empleado emp = new Empleado(id, nombre, puesto, salario);
+        empleados.add(emp);
+        return emp;
+    }
+
+    // Aqui ya utilizamos el patron para clonar a empleado
+    public Empleado clonarEmpleado(Empleado original, int nuevoId) {
+        Empleado copia = (Empleado) original.clonar();
+        copia.setId(nuevoId); // se ajusta el id para que no se dupliquen
+        empleados.add(copia);
+        return copia;
+    }
+
+    public List<Empleado> obtenerEmpleados() {
+        return empleados;
+    }
 }
